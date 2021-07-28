1175be933573f7445dc78a45ec27ac8f53f395b7
Sunil0354 <sunil0354@gmail.com>
2021-07-28 10:47:58 +0530

    -> Newly created library

---
+++ SampleSunilLibrary/src/main/java/com/example/samplesunillibrary/SampleSunilLibrary.java
@@ -0,0 +1,12 @@
+package com.example.samplesunillibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class SampleSunilLibrary {
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+}
